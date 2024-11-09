15418280fda54aaf0803ef01a22ebfacab21081c
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-09 19:07:11 +0600

    findmax

---
+++ findMax.js
@@ -0,0 +1,8 @@
+const findMax = arr => {
+    return arr.reduce((max, current) => {
+        return current > max ? current : max;
+    }, arr[5]);
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(findMax(array)); // Output: 10
