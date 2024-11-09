be7baaa8837c887cc642cc111bd258403417f44f
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-09 23:37:54 +0600

    anyNegative

---
+++ anyNegative.js
@@ -0,0 +1,7 @@
+function anyNegative(arr) {
+    return arr.some(num => num < 0);
+}
+
+
+console.log(anyNegative([1, 2, 3, -4, -5])); // Output: true
+console.log(anyNegative([1, 2, 3, 4, 5]));  // Output: false
