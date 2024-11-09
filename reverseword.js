db7ac9738c4117a6ec9e849578f074ef1f3cc0f9
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-09 18:05:00 +0600

    reverese word

---
+++ reverseword.js
@@ -0,0 +1,11 @@
+const reverseWord = sentence => {
+    let words = sentence.split(" ");
+    console.log(words);
+
+    const reveresed = words.map((word) => word.split("").reverse().join('')).join(' ');
+
+
+    console.log(reveresed);
+}
+
+reverseWord("I love You"); // Output: "I evol uoy"
